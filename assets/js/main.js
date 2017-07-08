88873cbe9e602f6405ba0be0830ecd89f703bcc4
Jesus Guerrero <jesusant.guerrero@gmail.com>
2017-07-07 23:18:25 -0400

    Model Octopus View

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,130 @@
+var model = {
+  init: function () {
+    this.cats = [{
+        id: 1,
+        name: 'Herbie',
+        clickCount: 0,
+        picture: 'https://i0.wallpaperscraft.com/image/cat_face_glasses_thick_65455_300x168.jpg'
+      },
+      {
+        id: 2,
+        name: 'Sheercat',
+        clickCount: 0,
+        picture: 'http://wallpapercave.com/wp/TxarQV1.jpg'
+      },
+      {
+        id: 3,
+        name: 'The twins',
+        clickCount: 0,
+        picture: 'https://images6.alphacoders.com/334/334727.jpg'
+      },
+      {
+        id: 4,
+        name: 'BoredCat',
+        clickCount: 0,
+        picture: 'http://cdn.wallpapersafari.com/99/42/EU5PQL.jpg'
+      },
+      {
+        id: 5,
+        name: 'Ninja Cat',
+        clickCount: 0,
+        picture: 'http://www.wallpapersfans.com/wp-content/uploads/2016/11/ninja-cat-fight-funny_wallpaper.jpg'
+      }
+    ]
+  },
 
-var catItem = $(".cat-item");
-var displayItem = $(".cat-display")
-var activeCat = $('.active-cat')
-var activeCatClicks = displayItem.find('.clicks')
+  getAllCats: function(){
+    return this.cats
+  },
 
-$("#cat-name1").text("Harbie")
-$("#cat-name2").text("Sheercat")
+  getCat: function(name){
+    this.activeCat = ''
+    this.cats.forEach(function(cat){
+      if(cat.name == name){
+        activeCat = cat
+      }
+    })
+    return activeCat
+  },
 
-displaySelected()
-
-catItem.on('click',function(){
-  catItem.removeClass("selected")
-  $(this).addClass("selected")
-  displaySelected()
-})
-
-activeCat.on('click',function(){
-  var element = $(this)
-  var elementId = element.attr('id')
-  $()
-  setCatScale(element,"1.1")
-  setTimeout(()=>{
-    setCatScale(element,"1.01")
-  },100)
-  updateClicks()
-})
-
-function setCatScale(el,scale){
-  el.css({transform:`scale(${scale})`});
+  updateClickCount: function(id){
+    var index = id - 1
+    this.cats[index].clickCount++
+    displayView.init(this.cats[index])
+  }
 }
 
-function displaySelected(){
-  var selectedCat = $('.cat-item.selected')
-  var src = selectedCat.find('img').attr('src')
-  displayItem.find('img').attr("src",src)
-  getClicks()
+var octopus = {
+  getCats: function(){
+    return model.getAllCats();
+  },
+
+  updateDisplay: function(name){
+    var activeCat =  model.getCat(name)
+    displayView.init(activeCat)
+  },
+
+  updateClick: function(id){
+    model.updateClickCount(id)
+  },
+
+  init: function(){
+    model.init()
+    catListView.init()
+    octopus.clickItem()
+  }
 }
 
-function updateClicks(){
-  var el = $('.cat-item.selected')
-  var clicksCount = el.find(".clicks")
-  var clicks = Number(clicksCount.text()) + 1
-  clicksCount.text(clicks)
-  activeCatClicks.text(clicks)
+var displayView = {
+  init: function(cat){
+    this.$display = $('.game-container');
+    displayView.render(cat)
+  },
+  render: function(cat){
+    var activeCat = `
+      <img class="cats active-cat" src="${cat.picture}"/>
+      <p class="cat-name">${cat.name}</p>
+      <p class="click-number"> <span class="clicks">${cat.clickCount}</span><span class="text">Clicks</span></p>`
+   this.$display.html(activeCat)
+   displayView.clickHandler(cat.id)
+  },
+  clickHandler: function(id){
+  
+    this.activeCatId = id
+    var activeCat = $(".active-cat")
+    activeCat.on('click',function(){
+      octopus.updateClick(id)
+    })
+  }
 }
 
-function getClicks(){
-  var el = $('.cat-item.selected')
-  var clicksCount = el.find(".clicks")
-  var clicks = clicksCount.text()
-  activeCatClicks.text(clicks)
+var catListView = {
+  init: function(){
+    this.$catList = $('.cat-list');
+    catListView.render()
+  },
+
+  render: function(){
+    this.catItems = ' '
+    octopus.getCats().forEach(function(cat) {
+      this.catItems += `<li class="cat-item"><img class="cats" src="${cat.picture}" />
+        <p class="name">${cat.name}</p>
+      </li>`
+    }, this);
+    this.$catList.html(this.catItems);
+    catListView.clickHandler()
+  },
+  
+  clickHandler: function(){
+    var catItem = $(".cat-item")
+
+    catItem.on('click', function () {
+      var $this = $(this)
+      catItem.removeClass("selected")
+      $this.addClass("selected")
+      octopus.updateDisplay($this.find('.name').text())
+    })
+  },
 }
+
+octopus.init();
